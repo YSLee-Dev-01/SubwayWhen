5246afadf4754bf36166e6bec5653067388e615c
YSLee-Dev <ysleedev01@gmail.com>
2025-07-18 11:02:18 +0900

    Feat(Report): 첫번째 질문 뷰 추가

---
+++ SubwayWhen/Presentation/Report/Sub/ReportFirstQuestionView.swift
@@ -0,0 +1,19 @@
+//
+//  ReportFirstQuestionView.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 7/18/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ReportFirstQuestionView: View {
+    var body: some View {
+        Text("ReportFirstQuestionView")
+    }
+}
+
+#Preview {
+    ReportFirstQuestionView()
+}
