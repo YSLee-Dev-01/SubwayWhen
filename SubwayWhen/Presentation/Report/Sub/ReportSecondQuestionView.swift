156521fb5fe324bd7bf23a44c12b2d85bb0b25c0
YSLee-Dev <ysleedev01@gmail.com>
2025-07-21 09:24:54 +0900

    Feat(Report): 2번째 질문 View 파일 추가

---
+++ SubwayWhen/Presentation/Report/Sub/ReportSecondQuestionView.swift
@@ -0,0 +1,19 @@
+//
+//  ReportSecondQuestionView.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 7/21/25.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ReportSecondQuestionView: View {
+    var body: some View {
+        Text("ReportSecondQuestionView")
+    }
+}
+
+#Preview {
+    ReportSecondQuestionView()
+}
