f465323e6e20738ca44800d6ae011d9955d8e734
YSLee-Dev <ysleedev01@gmail.com>
2025-07-23 11:37:32 +0900

    Feat(Report): 세번째, 네번째 질문 View가 나온 경우 스크롤 되도록 변경, 코드 최적화

--- SubwayWhen/Presentation/Report/Entity/ReportContentData.swift
+++ SubwayWhen/Presentation/Report/Entity/ReportContentData.swift
@@ -11,13 +11,14 @@ struct ReportContentData: Equatable {
     let title : String
     let iconName : String
     let message : String
+    let iconSize: CGSize
     
     static let defaultDataList : [ReportContentData] = [
-        .init(title: Strings.Report.fourStepOptionTitle1, iconName: "thermometer", message: Strings.Report.fourStepOptionSub1),
-        .init(title: Strings.Report.fourStepOptionTitle2, iconName: "thermometer.snowflake", message: Strings.Report.fourStepOptionSub2),
-        .init(title: Strings.Report.fourStepOptionTitle3, iconName: "cart", message: Strings.Report.fourStepOptionSub3),
-        .init(title: Strings.Report.fourStepOptionTitle4, iconName: "figure.wave", message: Strings.Report.fourStepOptionSub4),
-        .init(title: Strings.Report.fourStepOptionTitle5, iconName: "figure.fall", message: Strings.Report.fourStepOptionSub5),
-        .init(title: Strings.Report.fourStepOptionTitle6, iconName: "tortoise", message: Strings.Report.fourStepOptionSub6)
+        .init(title: Strings.Report.fourStepOptionTitle1, iconName: "thermometer", message: Strings.Report.fourStepOptionSub1, iconSize: .init(width: 20, height: 25)),
+        .init(title: Strings.Report.fourStepOptionTitle2, iconName: "thermometer.snowflake", message: Strings.Report.fourStepOptionSub2, iconSize: .init(width: 25, height: 25)),
+        .init(title: Strings.Report.fourStepOptionTitle3, iconName: "cart", message: Strings.Report.fourStepOptionSub3, iconSize: .init(width: 25, height: 25)),
+        .init(title: Strings.Report.fourStepOptionTitle4, iconName: "figure.wave", message: Strings.Report.fourStepOptionSub4, iconSize: .init(width: 18, height: 25)),
+        .init(title: Strings.Report.fourStepOptionTitle5, iconName: "figure.fall", message: Strings.Report.fourStepOptionSub5, iconSize: .init(width: 25, height: 25)),
+        .init(title: Strings.Report.fourStepOptionTitle6, iconName: "tortoise", message: Strings.Report.fourStepOptionSub6, iconSize: .init(width: 30, height: 25)),
     ]
 }
--- SubwayWhen/Presentation/Report/ReportView.swift
+++ SubwayWhen/Presentation/Report/ReportView.swift
@@ -10,53 +10,63 @@ import ComposableArchitecture
 
 struct ReportView: View {
     @State private var store:  StoreOf<ReportFeature>
+    @Namespace private var threeStep
+    @Namespace private var fourStep
     
     init(store: StoreOf<ReportFeature>) {
         self.store = store
     }
     
     var body: some View {
-        NavigationBarScrollViewInSUI(
-            title: Strings.Report.title,
-            backBtnTapped: {
-                
-            },
-            backBtnIcon: "arrow.left"
-        ) {
-            if store.reportStep >= 1 {
-                Group {
-                    ReportFirstQuestionView(store: self.$store)
-                        .transition(.offset(x: 0, y : -20).combined(with: .opacity))
+        ScrollViewReader { proxy in
+            NavigationBarScrollViewInSUI(
+                title: Strings.Report.title,
+                backBtnTapped: {
+                    
+                },
+                backBtnIcon: "arrow.left"
+            ) {
+                VStack(spacing: 20) {
+                    if store.reportStep >= 1 {
+                        ReportFirstQuestionView(store: self.$store)
+                            .transition(.offset(x: 0, y : -20).combined(with: .opacity))
+                    }
+                    
+                    if store.reportStep >= 2 {
+                        ReportSecondQuestionView(store: self.$store)
+                            .transition(.offset(x: 0, y : -20).combined(with: .opacity))
+                    }
+                    
+                    if store.reportStep >= 3 {
+                        ReportThreeQuestionView(store: self.$store)
+                            .id(threeStep)
+                            .transition(.offset(x: 0, y : -20).combined(with: .opacity))
+                    }
+                    
+                    if store.reportStep >= 4 {
+                        ReportFourQuestionView(store: self.$store)
+                            .id(fourStep)
+                            .transition(.offset(x: 0, y : -20).combined(with: .opacity))
+                    }
                 }
-                .padding(.bottom, 20)
             }
-            
-            if store.reportStep >= 2 {
-                Group {
-                    ReportSecondQuestionView(store: self.$store)
-                        .transition(.offset(x: 0, y : -20).combined(with: .opacity))
+            .onChange(of: self.store.reportStep, initial: false) { _, step  in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        switch step {
+                        case 3:
+                            proxy.scrollTo(threeStep, anchor: .center)
+                        case 4:
+                            proxy.scrollTo(fourStep, anchor: .center)
+                        default:
+                            break
+                        }
+                    }
                 }
-                .padding(.bottom, 20)
             }
-            
-            if store.reportStep >= 3 {
-                Group {
-                    ReportThreeQuestionView(store: self.$store)
-                        .transition(.offset(x: 0, y : -20).combined(with: .opacity))
-                }
-                .padding(.bottom, 20)
-            }
-            
-            if store.reportStep >= 4 {
-                Group {
-                    ReportFourQuestionView(store: self.$store)
-                        .transition(.offset(x: 0, y : -20).combined(with: .opacity))
-                }
-                .padding(.bottom, 20)
-            }
+            .animation(.smooth(duration: 0.3), value: self.store.reportStep)
+            .animation(.smooth(duration: 0.3), value: self.store.insertingData)
         }
-        .animation(.smooth(duration: 0.3), value: self.store.reportStep)
-        .animation(.smooth(duration: 0.3), value: self.store.insertingData)
         .confirmationDialog(self.$store.scope(state: \.dialogState, action: \.dialogAction))
         .onAppear {
             self.store.send(.onAppear)
