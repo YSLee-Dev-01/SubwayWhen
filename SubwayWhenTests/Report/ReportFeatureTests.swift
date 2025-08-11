75b89217800c6f4f9ffae5cbad33e3134df11d61
YSLee-Dev <ysleedev01@gmail.com>
2025-08-11 13:59:02 +0900

    Test(Report): TCA 기반 테스트 로직 파일 추가

---
+++ SubwayWhenTests/Report/ReportFeatureTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReportFeatureTests.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 8/11/25.
+//
+
+import XCTest
+import ComposableArchitecture
+
+@testable import SubwayWhen
+
+final class ReportFeatureTests: XCTestCase {
+    
+}
+
