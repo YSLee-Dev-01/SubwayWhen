34c439b7ea8e392c8d1adcf014e9e6727d51af6b
YSLee-Dev <ysleedev01@gmail.com>
2025-09-24 21:55:19 +0900

    Test(Setting): SettingFeature를 테스트할 파일 추가 및 init 테스트, 출퇴근 시간 테스트 추가

---
+++ SubwayWhenTests/Setting/SettingFeatureTests.swift
@@ -0,0 +1,89 @@
+//
+//  SettingFeatureTests.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 9/24/25.
+//
+
+import Foundation
+import XCTest
+import ComposableArchitecture
+
+@testable import SubwayWhen
+
+class SettingFeatureTests: XCTest {
+    
+    override func setUp()  {
+        FixInfo.saveSetting.mainGroupOneTime = 0
+        FixInfo.saveSetting.mainGroupTwoTime = 0
+    }
+    
+    func testSettingInit() async throws {
+        // GIVEN
+        let testStore = await self.createStore()
+        
+        // WHEN 
+        await testStore.send(.onAppear) { state in
+            // THEN - View가 처음 로드 되었을 때 (모든 값은 기본 값, 설정 값)
+            state.settingSections = [
+                .init(title: Strings.Setting.homeScreen, cellList: [
+                    SettingViewCell(title: "", type: .time),
+                    SettingViewCell(title: Strings.Setting.workAlarm, type: .navigation(.notiModal)),
+                    SettingViewCell(title: Strings.Setting.trafficLightEmoji, type: .textField(\.mainCongestionLabel))
+                ]),
+                .init(title: Strings.Setting.detailScreen, cellList: [
+                    SettingViewCell(title: Strings.Setting.autoRefresh, type: .toggle(\.detailAutoReload)),
+                    SettingViewCell(title: Strings.Setting.autoSortTimeTable, type: .toggle(\.detailScheduleAutoTime)),
+                    SettingViewCell(title: Strings.Setting.liveActivity, type: .toggle(\.liveActivity)),
+                    SettingViewCell(title: Strings.Setting.trainIcon, type: .navigation(.trainIcon))
+                ]),
+                .init(title: Strings.Setting.searchScreen, cellList: [
+                    SettingViewCell(title: Strings.Setting.duplicatePrevention, type: .toggle(\.searchOverlapAlert))
+                ]),
+                .init(title: Strings.Setting.other, cellList: [
+                    SettingViewCell(title: Strings.Setting.openLicense, type: .navigation(.licenseModal)),
+                    SettingViewCell(title: Strings.Setting.other, type: .navigation(.contentsModal))
+                ])
+            ]
+            state.selectedTimeViewType = nil
+            state.savedSettings = FixInfo.saveSetting
+        }
+    }
+    
+    func testTimeViewTapped() async throws {
+        // GIVEN
+        let testStore = await self.createStore()
+        
+        // THEN
+        await testStore.send(.timeViewTapped(.work)) { state in
+            // THEN - 타입이 출근으로 변경되어야 함
+            state.selectedTimeViewType = .work
+        }
+        
+        await testStore.send(.timeViewTapped(.leave)) { state in
+            // THEN - 타입이 퇴근으로 변경되어야 함
+            state.selectedTimeViewType = .leave
+        }
+        
+        await testStore.send(.timeSaveBtnTapped(21)) { state in
+            // THEN - 타입이 제거 되어야 하며, 퇴근시간은 아직 이전 시간을 유지함 (퇴근시간의 시간이 21시은 다음 이벤트)
+            state.selectedTimeViewType = nil
+            state.savedSettings.mainGroupTwoTime = 0
+        }
+        
+        await testStore.receive(.updateSavedSettings) { state in
+            // THEN - 퇴근시간의 시간이 21시로 업데이트 되어야 함
+            state.savedSettings.mainGroupTwoTime = 21
+        }
+    }
+}
+
+private extension SettingFeatureTests {
+    func createStore() async -> TestStoreOf<SettingFeature> {
+        let store = await TestStore(initialState: SettingFeature.State(), reducer: {
+            SettingFeature()
+        })
+        store.exhaustivity = .off(showSkippedAssertions: false)
+        return store
+    }
+}
