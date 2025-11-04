1c4f74ccf51ad58ca0ce938fd1af2adaa3a5706e
YSLee-Dev <ysleedev01@gmail.com>
2025-11-04 15:16:31 +0900

    Feat(Main): 혼잡도, 편집, 민원 출퇴근 버튼 등을 담은 MainTableHeaderView 레이아웃 구성 및 기본 설계

--- SubwayWhen/Presentation/Main/MainTable/Sub/MainTableViewHeader/MainTableHeaderView.swift
+++ SubwayWhen/Presentation/Main/MainTable/Sub/MainTableViewHeader/MainTableHeaderView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Then
+import SnapKit
 import RxSwift
 import RxCocoa
 
@@ -14,10 +15,36 @@ class MainTableHeaderView: UIView {
     
     // MARK: - Properties
     
+    private let congestionLabelBG = MainTableViewHeaderView(
+        title: Strings.Main.currentTraffic, subTitle: "🫥🫥🫥🫥🫥🫥🫥🫥🫥🫥"
+    )
+    
+    private let importantLabelBG = MainTableViewHeaderView (
+        title: Strings.Main.importantAlarm, subTitle: Strings.Main.importantAlarm
+    )
+    
+    private let reportBtn = MainTableViewHeaderBtn(title: Strings.Report.title, img: "Report")
+    private let editBtn = MainTableViewHeaderBtn(title: Strings.Main.edit, img: "List")
+    
+    private let liveStatusLabel = UILabel().then {
+        $0.font = .boldSystemFont(ofSize: ViewStyle.FontSize.largeSize)
+        $0.textColor = UIColor.gray
+        $0.text = Strings.Main.liveStatus
+    }
+    
+    private let groupView = MainTableHeaderGroupView()
+    
+    private let bag = DisposeBag()
+    
     // MARK: - LifeCycle
     
     init() {
-        super.init(frame: .zero)
+        super.init(frame: CGRect(x: 0, y: 0, width: 100 , height: 300))
+        
+        self.attribute()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { // 레이아웃 오류 방지
+            self.layout()
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -29,5 +56,44 @@ class MainTableHeaderView: UIView {
 // MARK: - Methods
 
 extension MainTableHeaderView {
+    private func attribute() {
+        self.backgroundColor = .systemBackground
+    }
     
+    private func layout() {
+        self.addSubviews(
+            self.congestionLabelBG,
+            self.editBtn, self.reportBtn,
+            self.liveStatusLabel, self.groupView
+        )
+        self.congestionLabelBG.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(ViewStyle.padding.mainStyleViewLR)
+            $0.height.equalTo(90)
+        }
+        
+        self.reportBtn.snp.makeConstraints{
+            $0.top.equalTo(self.congestionLabelBG.snp.bottom).offset(10)
+            $0.leading.equalTo(self.congestionLabelBG)
+            $0.trailing.equalTo(self.congestionLabelBG.snp.centerX).offset(-5)
+            $0.height.equalTo(90)
+        }
+        
+        self.editBtn.snp.makeConstraints{
+            $0.top.equalTo(self.congestionLabelBG.snp.bottom).offset(10)
+            $0.trailing.equalTo(self.congestionLabelBG)
+            $0.leading.equalTo(self.congestionLabelBG.snp.centerX).offset(5)
+            $0.height.equalTo(90)
+        }
+        
+        self.liveStatusLabel.snp.makeConstraints{
+            $0.top.equalTo(self.editBtn.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(ViewStyle.padding.mainStyleViewLR)
+        }
+        
+        self.groupView.snp.makeConstraints{ // height = 40
+            $0.top.equalTo(self.liveStatusLabel.snp.bottom).offset(10)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 }
