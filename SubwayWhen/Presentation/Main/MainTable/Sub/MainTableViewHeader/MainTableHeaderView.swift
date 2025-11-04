ad698e2df34be478cedbef9c0db04f6db74f8134
YSLee-Dev <ysleedev01@gmail.com>
2025-11-04 13:35:14 +0900

    Feat(Main): MainTableViewGroupCell, MainTableViewHeaderCell을 대채할 MainTableHeaderView 정의

---
+++ SubwayWhen/Presentation/Main/MainTable/Sub/MainTableViewHeader/MainTableHeaderView.swift
@@ -0,0 +1,33 @@
+//
+//  MainTableHeaderView.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 11/4/25.
+//
+
+import UIKit
+import Then
+import RxSwift
+import RxCocoa
+
+class MainTableHeaderView: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+// MARK: - Methods
+
+extension MainTableHeaderView {
+    
+}
