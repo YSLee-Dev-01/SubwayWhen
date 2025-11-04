057b213830be87921c97428912f722ce65bf495d
YSLee-Dev <ysleedev01@gmail.com>
2025-11-04 15:45:39 +0900

    Feat(Main): MainTableHeaderGroupView에 bind 추가 및 action 매핑

--- SubwayWhen/Presentation/Main/MainTable/Sub/MainTableViewHeader/MainTableHeaderGroupView.swift
+++ SubwayWhen/Presentation/Main/MainTable/Sub/MainTableViewHeader/MainTableHeaderGroupView.swift
@@ -27,6 +27,7 @@ class MainTableHeaderGroupView: UIView {
         $0.unSeleted()
     }
     
+    private let mainTableViewAction = PublishRelay<MainViewAction>()
     private let bag = DisposeBag()
     
     // MARK: - LifeCycle
@@ -36,6 +37,7 @@ class MainTableHeaderGroupView: UIView {
         
         self.attribute()
         self.layout()
+        self.bind()
     }
     
     required init?(coder: NSCoder) {
@@ -70,5 +72,58 @@ extension MainTableHeaderGroupView {
             $0.top.bottom.equalToSuperview()
             $0.leading.equalTo(self.groupOne.snp.trailing)
         }
+    }
+    
+    private func bind() {
+        Observable.merge(
+            self.groupOne.rx.tap.map { _ in SaveStationGroup.one},
+            self.groupTwo.rx.tap.map { _ in SaveStationGroup.two}
+        )
+        .do(onNext: {[weak self] group in
+            self?.updateGroupSelection(group)
+            self?.btnClickSizeChange(group: group == .two)
+        })
+        .map {.groupTap($0)}
+        .bind(to: self.mainTableViewAction)
+        .disposed(by: self.bag)
+    }
+    
+    private func updateGroupSelection(_ group: SaveStationGroup) {
+        if group == .one {
+            self.groupOne.seleted()
+            self.groupTwo.unSeleted()
+        } else {
+            self.groupOne.unSeleted()
+            self.groupTwo.seleted()
+        }
+    }
+    
+    private func btnClickSizeChange(group : Bool){
+        if group {
+            self.groupOne.snp.remakeConstraints{
+                $0.leading.equalToSuperview()
+                $0.top.bottom.equalToSuperview()
+                $0.trailing.equalTo(self.groupView.snp.leading).inset(100)
+            }
+        } else {
+            self.groupOne.snp.remakeConstraints{
+                $0.leading.equalToSuperview()
+                $0.top.bottom.equalToSuperview()
+                $0.trailing.equalTo(self.groupView.snp.trailing).inset(100)
+            }
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0.75) {
+            self.layoutIfNeeded()
+        }
+    }
+    
+    @discardableResult
+    func setDI(action: PublishRelay<MainViewAction>) -> Self {
+        self.mainTableViewAction
+            .bind(to: action)
+            .disposed(by: self.bag)
+        
+        return self
     }
 }
